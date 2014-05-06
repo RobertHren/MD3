5ac949f0b0a72eed1606d58c4ef3b886374d17af
Robert Dev <robert.skrlec@gmail.com>
2014-05-06 08:28:07 +0200

    md2

--- js/index_user_scripts.js
+++ js/index_user_scripts.js
@@ -8,17 +8,7 @@
  {
     
     
-         $(document).on("click", "#sidebar_btn", function(evt)
-        {
-         /* Other possible functions are: 
-           uib_sb.open_sidebar($sb)
-           uib_sb.close_sidebar($sb)
-           uib_sb.toggle_sidebar($sb)
-            uib_sb.close_all_sidebars()
-          See js/sidebar.js for the full sidebar API */
-        
-         uib_sb.toggle_sidebar($(".uib_w_27"));  
-        });
+         
 }
  $(document).ready(register_event_handlers);
 })();
